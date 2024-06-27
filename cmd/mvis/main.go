32c8c9530d405d613375b487cd6767f631152a84
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-27 13:20:44 +0200

    refresh

--- cmd/mvis/main.go
+++ cmd/mvis/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -10,5 +11,12 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Print(err)
 	}
+	// TODO: FIGURE OUT A WAY TO DO THIS PROGRAMATICALLY (WRITE TO ENV)
+	val, err := refreshAccessToken(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), os.Getenv("REFRESH_TOKEN"))
+	if err != nil {
+		log.Print("err getting token" + err.Error())
+	}
 
+	log.Print()
+	log.Print(val)
 }
