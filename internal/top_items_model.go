174be3571ff3245530a65a4a39572647d8cffc23
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-27 23:58:47 +0200

    resp model

---
+++ internal/top_items_model.go
@@ -0,0 +1,19 @@
+package internal
+
+type SpotifyResponse struct {
+	Next  string `json:"next"`
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Album struct {
+		Name string `json:"name"`
+	} `json:"album"`
+	Name       string   `json:"name"`
+	Artists    []Artist `json:"artists"`
+	DurationMs int      `json:"duration_ms"`
+}
+
+type Artist struct {
+	Name string `json:"name"`
+}
