09cea1114275350052f12ebe9aaf59d359ecd60a
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-28 23:55:03 +0200

    insert data into db

--- cmd/mvis/main.go
+++ cmd/mvis/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
+	model "github.com/Darth-Tenebros/Melodic-Visions/internal/model"
+	repository "github.com/Darth-Tenebros/Melodic-Visions/internal/repository"
 	"github.com/Darth-Tenebros/Melodic-Visions/internal/spotify"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -34,18 +38,25 @@ func main() {
 		fmt.Println(err)
 	}
 
+	database, err := sql.Open("sqlite3", "/home/yolisa/Documents/Projects/Melodic-Visions/data/spotify_data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	track_repo := repository.NewTrackRepository(database)
+
 	time_listened := 0
+	var tracks []model.Track
 	for {
 
-		for _, item := range result.Items {
-			time_listened = item.DurationMs + time_listened
-			spotify.Write_file(fmt.Sprintf("%s <:> %s", item.Name, item.Artists))
-		}
-		fmt.Println("SUCCESS!!")
+		data := convertItemsToTracks(result.Items)
+		tracks = append(tracks, data...)
 
 		if err != nil {
 			log.Print(err)
 		}
+		fmt.Println("SUCCESS")
 
 		fmt.Println(result.Next)
 		if strings.Contains(result.Next, "http") {
@@ -56,9 +67,38 @@ func main() {
 		}
 	}
 
+	for _, track := range tracks {
+		_, err := track_repo.InsertTrack("long_term", track)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
 	fmt.Println(time_listened)
 	duration := time.Duration(time_listened) * time.Millisecond
 	fmt.Printf("your listened for %f minutes\n", duration.Minutes())
 	fmt.Printf("whch is %f hours", duration.Hours())
 
+}
+
+func convertItemToTrack(item model.Item) model.Track {
+	artistName := ""
+	if len(item.Artists) > 0 {
+		artistName = item.Artists[0].Name
+	}
+
+	return model.Track{
+		TrackName:  item.Name,
+		AlbumName:  item.Album.Name,
+		Duration:   item.DurationMs,
+		ArtistName: artistName,
+	}
+}
+
+func convertItemsToTracks(items []model.Item) []model.Track {
+	tracks := make([]model.Track, len(items))
+	for i, item := range items {
+		tracks[i] = convertItemToTrack(item)
+	}
+	return tracks
 }
