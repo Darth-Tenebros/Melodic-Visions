a88bc4ef241ce5941c94582971db68b6171d2bf3
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-07-08 20:42:50 +0200

    move bar chart impl

---
+++ internal/render_charts/bar_chart.go
@@ -0,0 +1,26 @@
+package render_charts
+
+import (
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+// TODO: CLEAN UP RENDERING
+func generateBarItems(values []int) []opts.BarData {
+	items := make([]opts.BarData, 0)
+	for i := 0; i < len(values); i++ {
+		items = append(items, opts.BarData{Value: values[i]})
+	}
+	return items
+}
+
+func BarBasic(keys []string, values []int) *charts.Bar {
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{Title: "basic bar example", Subtitle: "This is the subtitle."}),
+	)
+
+	bar.SetXAxis(keys).
+		AddSeries("Category A", generateBarItems(values))
+	return bar
+}
