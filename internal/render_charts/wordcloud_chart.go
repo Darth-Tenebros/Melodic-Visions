15755304b10e0f15bf34c0cddfdb37d9ecf4243e
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-07-13 22:06:03 +0200

    generate wordcloud chart

---
+++ internal/render_charts/wordcloud_chart.go
@@ -0,0 +1,31 @@
+package render_charts
+
+import (
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func generateWCData(data map[string]interface{}) (items []opts.WordCloudData) {
+	items = make([]opts.WordCloudData, 0)
+	for k, v := range data {
+		items = append(items, opts.WordCloudData{Name: k, Value: v})
+	}
+	return
+}
+
+func WordCloudBasic(data map[string]interface{}) *charts.WordCloud {
+	wc := charts.NewWordCloud()
+	wc.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "basic WordCloud example",
+		}))
+
+	wc.AddSeries("wordcloud", generateWCData(data)).
+		SetSeriesOptions(
+			charts.WithWorldCloudChartOpts(
+				opts.WordCloudChart{
+					SizeRange: []float32{14, 80},
+				}),
+		)
+	return wc
+}
